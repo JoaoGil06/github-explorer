14926345c6f7f6ba5f22249e5e19c3c7c5bfdf95
joaogil06 <Joaogmonteiro06@gmail.com>
2020-05-05 20:02:55 +0100

    Styled Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+
+import { Title, Form, Repositories } from './styles'
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digita o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+            <img src="https://avatars0.githubusercontent.com/u/33355337?s=460&u=3c357e3a6e02e6aefbfe745db0a1af594f9906d7&v=4"
+            alt="João Gil" />
+            <div>
+              <strong>Repositório 01</strong>
+              <p>Descrição Repositório</p>
+            </div>
+
+            <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+            <img src="https://avatars0.githubusercontent.com/u/33355337?s=460&u=3c357e3a6e02e6aefbfe745db0a1af594f9906d7&v=4"
+            alt="João Gil" />
+            <div>
+              <strong>Repositório 01</strong>
+              <p>Descrição Repositório</p>
+            </div>
+
+            <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+            <img src="https://avatars0.githubusercontent.com/u/33355337?s=460&u=3c357e3a6e02e6aefbfe745db0a1af594f9906d7&v=4"
+            alt="João Gil" />
+            <div>
+              <strong>Repositório 01</strong>
+              <p>Descrição Repositório</p>
+            </div>
+
+            <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   )
 }
 
